Zen-Vora/gradle

Reduce the usage of embedded executor

.teamcity/src/test/kotlin/BuildTypeTest.kt
@@ -76,7 +76,7 @@ class BuildTypeTest {
             "TestFunctionalTest",
             "Test Functional Test",
             "Test Functional Test",
-            TestCoverage(4, TestType.platform, Os.WINDOWS, JvmVersion.java23, JvmVendor.openjdk),
+            TestCoverage(4, TestType.quick, Os.WINDOWS, JvmVersion.java23, JvmVendor.openjdk),
             buildModel.stages[2]
         )
         val gradleStep = functionalTest.steps.getGradleStep(GRADLE_RUNNER_STEP_NAME)
@@ -90,7 +90,7 @@ class BuildTypeTest {
                 "%additional.gradle.parameters%",
                 "--daemon",
                 "--continue",
-                "-DbuildScan.PartOf=PlatformJava23AdoptiumWindowsAmd64,PullRequestFeedback,ReadyforNightly,ReadyforRelease",
+                "-DbuildScan.PartOf=QuickJava23AdoptiumWindowsAmd64,PullRequestFeedback,ReadyforNightly,ReadyforRelease",
                 "-PtestJavaVersion=23",
                 "-PtestJavaVendor=openjdk",
                 "-Dscan.tag.FunctionalTest",

omeos-fork/gradle.gradle

.teamcity/src/test/kotlin/BuildTypeTest.kt
@@ -86,7 +86,7 @@ class BuildTypeTest {
                 "TestFunctionalTest",
                 "Test Functional Test",
                 "Test Functional Test",
-                TestCoverage(4, TestType.PLATFORM, Os.WINDOWS, JvmVersion.JAVA_23, JvmVendor.OPENJDK),
+                TestCoverage(4, TestType.QUICK, Os.WINDOWS, JvmVersion.JAVA_23, JvmVendor.OPENJDK),
                 buildModel.stages[2],
             )
 
@@ -109,7 +109,7 @@ class BuildTypeTest {
                 "%additional.gradle.parameters%",
                 "--daemon",
                 "--continue",
-                "-DbuildScan.PartOf=PlatformJava23AdoptiumWindowsAmd64,PullRequestFeedback,ReadyforNightly,ReadyforRelease",
+                "-DbuildScan.PartOf=QuickJava23AdoptiumWindowsAmd64,PullRequestFeedback,ReadyforNightly,ReadyforRelease",
                 "-PtestJavaVersion=23",
                 "-PtestJavaVendor=openjdk",
                 "-Dscan.tag.FunctionalTest",